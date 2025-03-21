fa84614e8bc5a324a30ace8a486bbb838446ff93
Dhananjay Bhatia <dhananjaybhatia@yahoo.co.in>
2025-03-21 12:05:43 +1100

    Edit "NOW" animation

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,11 @@
 let tl = gsap.timeline();
 
-tl.from(".line h1", {
+tl.from(".line h1, .line h2", {
   y: 150,
   stagger: 0.25,
   duration: 0.6,
   delay: 0.5,
+  opacity: 0,
 });
 tl.from("#line1-part1, .line h2", {
   opacity: 0,
@@ -18,12 +19,25 @@ tl.from("#line1-part1, .line h2", {
         h5Timer.innerHTML = grow;
         console.log(grow);
       }
-    }, 35);
+    }, 25);
   },
 });
 
 tl.to("#loader", {
   opacity: 0,
-  delay: 4,
-  duration: 0.4,
+  delay: 2.8,
+  duration: 0.3,
 });
+
+tl.from("#page1", {
+  delay: 0.3,
+  y: 1600,
+  opacity: 0,
+  ease: Power4,
+});
+
+tl.to("#loader", {
+  display: "none",
+});
+
+
