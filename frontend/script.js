d36248e0164e8c9383b64eb9e7cbfeb420acd668
Dhananjay Bhatia <dhananjaybhatia@yahoo.co.in>
2025-03-24 11:55:05 +1100

    page 1 done

--- frontend/script.js
+++ frontend/script.js
@@ -1,43 +1,68 @@
-let tl = gsap.timeline();
+function loadingAnimation() {
+  let tl = gsap.timeline();
 
-tl.from(".line h1, .line h2", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
-  opacity: 0,
-});
-tl.from("#line1-part1, .line h2", {
-  opacity: 0,
-  onStart: function () {
-    const h5Timer = document.querySelector("#line1-part1 h5");
-    let grow = 0;
-    setInterval(function () {
-      if (grow < 100) {
-        h5Timer.innerHTML = grow++;
-      } else {
-        h5Timer.innerHTML = grow;
-        console.log(grow);
-      }
-    }, 25);
-  },
-});
+  tl.from(".line h1, .line h2", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+    opacity: 0,
+  });
 
-tl.to("#loader", {
-  opacity: 0,
-  delay: 2.8,
-  duration: 0.3,
-});
+  tl.from("#line1-part1, .line h2", {
+    opacity: 0,
+    onStart: function () {
+      const h5Timer = document.querySelector("#line1-part1 h5");
+      let grow = 0;
 
-tl.from("#page1", {
-  delay: 0.3,
-  y: 1600,
-  opacity: 0,
-  ease: Power4,
-});
+      const interval = setInterval(function () {
+        if (grow < 100) {
+          h5Timer.innerHTML = grow++;
+        } else {
+          h5Timer.innerHTML = grow;
 
-tl.to("#loader", {
-  display: "none",
-});
+          clearInterval(interval);
+        }
+      }, 25);
+    },
+  });
 
+  tl.to("#loader", {
+    opacity: 0,
+    delay: 1.9,
+    duration: 0.3,
+  });
 
+  tl.from("#page1", {
+    delay: 0.3,
+    y: 1600,
+    opacity: 0,
+    ease: Power4,
+  });
+
+  tl.to("#loader", {
+    display: "none",
+  });
+  tl.from(".hero h1, #hero3 h2", {
+    y: 150,
+    stagger: 0.2,
+  });
+  tl.from("#nav", {
+    opacity: 0,
+  });
+}
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (debts) {
+    console.log(debts.x);
+    gsap.to("#crsr", {
+      left: debts.x,
+      top: debts.y,
+    });
+  });
+
+  Shery.makeMagnet("#nav-part2 h4", {});
+}
+
+loadingAnimation();
+cursorAnimation();
