765fcef0cfce7783a86a4cd79f91cab24400f574
Dhananjay Bhatia <dhananjaybhatia@yahoo.co.in>
2025-04-02 12:02:36 +1100

    Page nums and Locomotive smooth trigger added

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,43 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
 function loadingAnimation() {
   let tl = gsap.timeline();
 
@@ -40,6 +80,9 @@ function loadingAnimation() {
     ease: Power4,
   });
 
+  // tl.to("#loader", {
+  //   display: "none",
+  // });
   tl.to("#loader", {
     display: "none",
   });
@@ -48,9 +91,9 @@ function loadingAnimation() {
     opacity: 0,
     stagger: 0.2,
     duration: 1,
-    ease: "power4.out"
+    ease: "power4.out",
   });
-  
+
   tl.from("#nav", {
     opacity: 0,
   });
@@ -67,6 +110,6 @@ function cursorAnimation() {
 
   Shery.makeMagnet("#nav-part2 h4", {});
 }
-
 loadingAnimation();
-cursorAnimation();
+// cursorAnimation();
+locomotiveAnimation();
